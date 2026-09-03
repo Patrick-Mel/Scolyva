e7412c857ef6fecfab4e1252733fef5e56eb6953
Patrick Melaga <patrickmelago17@gmail.com>
2026-09-03 13:21:45 +0100

    Fix WhiteNoise 500 Server Error on Render by using CompressedStaticFilesStorage

--- backend/scolyva_backend/settings.py
+++ backend/scolyva_backend/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
